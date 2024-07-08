d766918b4cbe6a365725454a9281c7a2b26bbfe5
Roman Schuka <pazitivnyi@gmail.com>
2024-07-08 22:55:23 +0300

    db removed

--- config.py
+++
@@ -1,6 +0,0 @@
-import os.path
-
-PROJECT_DIR = os.path.dirname(__file__)
-TEMPLATES_DIR = os.path.join(PROJECT_DIR, "templates")
-MIGRATIONS_DIR = os.path.join(PROJECT_DIR, "db", "migrations")
-DB_FILENAME = "database.sqlite"
