1f69085867bb16e2ed8265bff1d614f6e1eacc08
Roman Schuka <pazitivnyi@gmail.com>
2024-07-15 13:00:22 +0300

    template tiny changes

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from starlette.templating import Jinja2Templates
+
 from conf import TEMPLATES_DIR
 
 # Initialize Jinja2Templates with the directory containing the templates
 tpl = Jinja2Templates(directory=TEMPLATES_DIR)
+tpl.env.globals["current_year"] = lambda: datetime.utcnow().year
